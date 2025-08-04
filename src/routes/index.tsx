8d27570a7656dbac33958175ca0efec2fb60f125
istiak19 <istiak15-14128@diu.edu.bd>
2025-08-04 16:53:52 +0600

    project setup done

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+
+      }
+    ]
+  },
+]);
+
+export default router;
